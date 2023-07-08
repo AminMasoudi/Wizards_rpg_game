68a9058eadb73de5cf9e52b8c3134d2d8824f58d
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-07-08 09:06:50 +0330

    wtf??

--- server/api/routing.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/socket-server/', consumers.LobbyConsumer.as_asgi()),
-    # re_path(r"", get_asgi_application()),
-]
